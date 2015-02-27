55648bc111a4ea627fdc5bf9d11be6dfe4550b08
Timani Tunduwani <timani@users.noreply.github.com>
2015-02-26 23:05:20 -0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,7 +26,13 @@ echo "-----------------------------------"
 mkdir $HOME/.build/$PNAME
 mv $HOME/.build/pantheon-repo/.git $HOME/.build/$PNAME/.git
 cd $HOME/.build/$PNAME
-
+# git remote add pantheon ssh://codeserver.dev.$PUUID@codeserver.dev.$PUUID.drush.in:2222/~/repository.git 
+git config --global push.default simple
+git config --global user.email "timani27@gmail.com"
+git config --global user.name "Pantheon Timani"
+git pull pantheon wp-meetup
+git push pantheon wp-meetup 
+git log # fetch branch from server and checkout locally 
 # Output of the diff vs upstream.
 echo "-----------------------------------"
 echo "Here's the status change!"
